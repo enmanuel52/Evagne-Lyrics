6dd8adc8cb25da404fcdc7092148f4b5e12fb5c3
“enmanuel” <enmanueldelgado52@gmail.com>
2022-07-21 18:15:55 +0200

    splash added

---
+++ app/src/main/java/com/example/evagnelyrics/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.evagnelyrics
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        finish()
+    }
+}
