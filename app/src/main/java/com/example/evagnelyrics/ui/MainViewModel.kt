25988479e7665bf39b8b4e672a5130e0474525c0
“enmanuel” <enmanueldelgado52@gmail.com>
2022-12-29 21:20:16 +0100

    avoid override the database

--- app/src/main/java/com/example/evagnelyrics/ui/MainViewModel.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/MainViewModel.kt
@@ -3,32 +3,48 @@ package com.example.evagnelyrics.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.evagnelyrics.core.Items
-import com.example.evagnelyrics.domain.usecase.EraseDatabaseUC
+import com.example.evagnelyrics.domain.model.Lyric
+import com.example.evagnelyrics.domain.usecase.GetAllLyricsUC
 import com.example.evagnelyrics.domain.usecase.InsertAllLyricsUC
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val insertAllLyricsUC: InsertAllLyricsUC,
-    private val deleteAllLyricsUC: EraseDatabaseUC,
+    private val gellAllLyricsUC: GetAllLyricsUC
 ) : ViewModel() {
 
     private val _ready: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val ready get() = _ready.asStateFlow()
+
+    private val allLyricsUC = gellAllLyricsUC()
+    private val allLyricsState = MutableStateFlow<List<Lyric>>(emptyList())
 
     init {
         setDatabase()
     }
 
     private fun setDatabase() = viewModelScope.launch {
-        deleteAllLyricsUC()
+        allLyricsUC.collectLatest { lyrics ->
+            allLyricsState.update {
+                lyrics
+            }
+        }
         delay(1000)
-        insertAllLyricsUC(Items.songs.map { it.toDomain() })
+        Items.songs.map { it.toDomain() }.forEach { lyric ->
+            val safeTitles = allLyricsState.value.map { it.title }
+            if (!safeTitles.contains(lyric.title)) {
+                insertAllLyricsUC(listOf(lyric))
+            }
+        }
+
 
         _ready.value = true
     }
