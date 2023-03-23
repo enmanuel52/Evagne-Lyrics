729c16d09b08dd18d3df67b8d7646d5056c8ce14
“enmanuel” <enmanueldelgado52@gmail.com>
2023-03-23 23:22:33 +0100

    some animation on lyrics

--- app/src/main/java/com/example/evagnelyrics/ui/screen/song/SongScreen.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/screen/song/SongScreen.kt
@@ -1,6 +1,7 @@
 package com.example.evagnelyrics.ui.screen.song
 
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +14,9 @@ import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -21,7 +25,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.evagnelyrics.core.LocalNavController
 import com.example.evagnelyrics.core.dimen
-import com.example.evagnelyrics.ui.util.JustSlideFromRight
+import com.example.evagnelyrics.ui.util.Horizontally
+import com.example.evagnelyrics.ui.util.SlideInOutFrom
+import com.example.evagnelyrics.ui.util.Vertically
+import com.example.evagnelyrics.ui.util.Where
 
 @Composable
 fun SongScreen(
@@ -40,7 +47,8 @@ fun SongScreen(
         targetState = true
     }
 
-    JustSlideFromRight(
+    SlideInOutFrom(
+        where = Where.Horizontal(Horizontally.End),
         visibleState = visibleState,
         durationMillis = 400,
         delayMillis = 100,
@@ -62,17 +70,24 @@ fun SongScreen(
                     }
                 }
             )
-            Text(
-                text = song?.letter.orEmpty(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.h1,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(MaterialTheme.dimen.medium)
-                    .verticalScroll(rememberScrollState()),
-                color = if (MaterialTheme.colors.isLight) MaterialTheme.colors.primary
-                else MaterialTheme.colors.onBackground
-            )
+            SlideInOutFrom(
+                where = Where.Vertical(Vertically.Bottom),
+                visibleState = visibleState,
+                durationMillis = 1000,
+                delayMillis = 500,
+            ) {
+                Text(
+                    text = song?.letter.orEmpty(),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.h1,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(MaterialTheme.dimen.medium)
+                        .verticalScroll(rememberScrollState()),
+                    color = if (MaterialTheme.colors.isLight) MaterialTheme.colors.primary
+                    else MaterialTheme.colors.onBackground
+                )
+            }
 
         }
     }
