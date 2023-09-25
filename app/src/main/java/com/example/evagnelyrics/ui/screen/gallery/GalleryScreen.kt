4ba06ce3a670f01f82c7a97c340bfac02f29c93e
“enmanuel” <enmanueldelgado52@gmail.com>
2023-09-25 22:42:28 +0100

    removing animations from gallery and picture

--- app/src/main/java/com/example/evagnelyrics/ui/screen/gallery/GalleryScreen.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/screen/gallery/GalleryScreen.kt
@@ -1,7 +1,5 @@
 package com.example.evagnelyrics.ui.screen.gallery
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -20,101 +18,42 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.boundsInRoot
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavHostController
 import com.example.evagnelyrics.R
 import com.example.evagnelyrics.core.Items
 import com.example.evagnelyrics.core.LocalNavController
 import com.example.evagnelyrics.core.dimen
-import com.example.evagnelyrics.domain.model.MyBox
-import com.example.evagnelyrics.ui.MainViewModel
 import com.example.evagnelyrics.ui.navigation.Route
 import com.example.evagnelyrics.ui.theme.component.EvText
 import com.example.evagnelyrics.ui.theme.component.EvTextStyle
-import com.example.evagnelyrics.ui.util.Horizontally
-import com.example.evagnelyrics.ui.util.ScaleFromCenter
-import com.example.evagnelyrics.ui.util.SlideInOutFrom
-import com.example.evagnelyrics.ui.util.Where
-import kotlinx.coroutines.delay
-import org.koin.androidx.compose.koinViewModel
-import kotlin.math.roundToInt
 
 @Composable
 fun GalleryScreen(
     navController: NavHostController = LocalNavController.current!!,
-    mainViewModel: MainViewModel = koinViewModel()
 ) {
-
-    val onBack by remember {
-        mutableStateOf(mainViewModel.previous() == Route.Picture)
-    }
-
-    val visibleState = MutableTransitionState(onBack).apply {
-        if (!onBack) {
-            targetState = true
-        }
-    }
-
-    BackHandler(enabled = true) {
-        mainViewModel.popScreen()
-        navController.popBackStack()
-    }
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(key1 = lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_CREATE) {
-                mainViewModel.pushScreen(Route.Gallery)
-            }
-        }
-
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
-
-    SlideInOutFrom(
-        where = Where.Horizontal(Horizontally.End),
-        visibleState = visibleState,
-        durationMillis = 200
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            TopAppBar(
-                title = {
-                    EvText(
-                        resource = R.string.title_wallpapers,
-                        style = EvTextStyle.Head,
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(
+            title = {
+                EvText(
+                    resource = R.string.title_wallpapers,
+                    style = EvTextStyle.Head,
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = "back on song"
                     )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        mainViewModel.popScreen()
-                        navController.popBackStack()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.ArrowBack,
-                            contentDescription = "back on song"
-                        )
-                    }
                 }
-            )
-            PicturesList(navController, 200, onBack)
-        }
+            }
+        )
+        PicturesList(navController)
     }
 
 
@@ -123,16 +62,7 @@ fun GalleryScreen(
 @Composable
 private fun PicturesList(
     navController: NavHostController,
-    delay: Int,
-    onBack: Boolean,
-    mainViewModel: MainViewModel = koinViewModel()
 ) {
-
-    val listVisibility = remember {
-        mutableStateListOf<Boolean>()
-    }.apply {
-        addAll((1..5).map { false })
-    }
 
     LazyVerticalGrid(
         modifier = Modifier
@@ -143,51 +73,25 @@ private fun PicturesList(
         contentPadding = PaddingValues(all = MaterialTheme.dimen.verySmall)
     ) {
         items(Items.images.size) { index ->
-            ScaleFromCenter(
-                visible = if (onBack) true else listVisibility[index],
-                hasBounce = !onBack,
-            ) {
-                var myBox by remember {
-                    mutableStateOf(MyBox())
-                }
-
-                Image(
-                    painter = painterResource(
-                        id = when (index) {
-                            0 -> R.drawable.img1_7185
-                            1 -> R.drawable.img2_7190
-                            2 -> R.drawable.img3_7228
-                            3 -> R.drawable.img4_7236
-                            4 -> R.drawable.img5_7281
-                            else -> R.drawable.img1_7185
-                        }
-                    ),
-                    contentDescription = "image # $index",
-                    modifier = Modifier
-                        .aspectRatio(0.6625f)
-                        .clip(RoundedCornerShape(5))
-                        .clickable {
-                            mainViewModel.setBox(myBox)
-                            navController.navigate(Route.Picture.toString() + "/$index")
-                        }
-                        .onGloballyPositioned {
-                            val rect = it.boundsInRoot()
-                            myBox = MyBox(
-                                topLeftX = rect.topLeft.x.roundToInt(),
-                                topLeftY = rect.topLeft.y.roundToInt(),
-                                width = rect.width.roundToInt(),
-                                height = rect.height.roundToInt()
-                            )
-                        }
-                )
-            }
-        }
-    }
-
-    LaunchedEffect(key1 = true) {
-        (0..4).forEach {
-            delay(if (it == 0) 100 + delay.toLong() else 200)
-            listVisibility[it] = true
+            Image(
+                painter = painterResource(
+                    id = when (index) {
+                        0 -> R.drawable.img1_7185
+                        1 -> R.drawable.img2_7190
+                        2 -> R.drawable.img3_7228
+                        3 -> R.drawable.img4_7236
+                        4 -> R.drawable.img5_7281
+                        else -> R.drawable.img1_7185
+                    }
+                ),
+                contentDescription = "image # $index",
+                modifier = Modifier
+                    .aspectRatio(0.6625f)
+                    .clip(RoundedCornerShape(5))
+                    .clickable {
+                        navController.navigate(Route.Picture.toString() + "/$index")
+                    }
+            )
         }
     }
 }
--- app/src/main/java/com/example/evagnelyrics/ui/screen/picture/PictureScreen.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/screen/picture/PictureScreen.kt
@@ -1,8 +1,5 @@
 package com.example.evagnelyrics.ui.screen.picture
 
-import android.util.Log
-import androidx.activity.compose.BackHandler
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Icon
@@ -13,115 +10,74 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavHostController
-import com.example.evagnelyrics.EvagneLyricsApp.Companion.TAG
 import com.example.evagnelyrics.R
 import com.example.evagnelyrics.core.Items
 import com.example.evagnelyrics.core.LocalNavController
 import com.example.evagnelyrics.core.dimen
-import com.example.evagnelyrics.ui.MainViewModel
-import com.example.evagnelyrics.ui.navigation.Route
 import com.example.evagnelyrics.ui.theme.component.EvText
 import com.example.evagnelyrics.ui.theme.component.EvTextStyle
-import com.example.evagnelyrics.ui.util.SlideInOutFrom
-import com.example.evagnelyrics.ui.util.Vertically
-import com.example.evagnelyrics.ui.util.Where
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
-import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun PictureScreen(
     page: Int = 0,
     navController: NavHostController = LocalNavController.current!!,
-    mainViewModel: MainViewModel = koinViewModel()
 ) {
-
-    val visibleState = MutableTransitionState(false).apply {
-        targetState = true
-    }
-
-    BackHandler(enabled = true) {
-        Log.d(TAG, "PictureScreen: the fucking back pressed")
-        mainViewModel.popScreen()
-        navController.popBackStack()
-    }
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(key1 = lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_CREATE) {
-                mainViewModel.pushScreen(Route.Picture)
-            }
-        }
-
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
-
-    val myBox by mainViewModel.myBoxState.collectAsState()
-
-    SlideInOutFrom(where = Where.Vertical(Vertically.Bottom), visibleState = visibleState) {
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (toolbar, pager) = createRefs()
-            TopAppBar(
-                title = {
-                    EvText(
-                        resource = R.string.picture_title,
-                        style = EvTextStyle.Head,
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        mainViewModel.popScreen()
-                        navController.popBackStack()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.ArrowBack,
-                            contentDescription = "back arrow"
-                        )
-                    }
-                },
-                modifier = Modifier.constrainAs(toolbar) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    width = Dimension.fillToConstraints
-                    height = Dimension.wrapContent
-                }
-            )
-
-            val pagerState = rememberPagerState(page)
-            HorizontalPager(
-                modifier = Modifier.constrainAs(pager) {
-                    top.linkTo(toolbar.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                    width = Dimension.fillToConstraints
-                    height = Dimension.fillToConstraints
-                },
-                count = Items.images.size,
-                itemSpacing = MaterialTheme.dimen.verySmall,
-                state = pagerState,
-            ) { index ->
-                Image(
-                    painter = painterResource(Items.images[index]),
-                    contentDescription = "image $index",
-                    modifier = Modifier.fillMaxSize()
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (toolbar, pager) = createRefs()
+        TopAppBar(
+            title = {
+                EvText(
+                    resource = R.string.picture_title,
+                    style = EvTextStyle.Head,
                 )
+            },
+            navigationIcon = {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = "back arrow"
+                    )
+                }
+            },
+            modifier = Modifier.constrainAs(toolbar) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                width = Dimension.fillToConstraints
+                height = Dimension.wrapContent
             }
+        )
+
+        val pagerState = rememberPagerState(page)
+        HorizontalPager(
+            modifier = Modifier.constrainAs(pager) {
+                top.linkTo(toolbar.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            },
+            count = Items.images.size,
+            itemSpacing = MaterialTheme.dimen.verySmall,
+            state = pagerState,
+        ) { index ->
+            Image(
+                painter = painterResource(Items.images[index]),
+                contentDescription = "image $index",
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 
