44f725c55335c0986b44b473b4d3963fea26b84f
“enmanuel” <enmanueldelgado52@gmail.com>
2023-09-30 15:10:27 +0100

    some fixing on koin migration

--- app/src/main/java/com/example/evagnelyrics/ui/di/UiModule.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/di/UiModule.kt
@@ -1,19 +1,17 @@
 package com.example.evagnelyrics.ui.di
 
-import com.example.evagnelyrics.data.repo.ScreenStackImpl
-import com.example.evagnelyrics.domain.repo.ScreenStack
 import com.example.evagnelyrics.ui.MainViewModel
 import com.example.evagnelyrics.ui.screen.list.ListViewModel
 import com.example.evagnelyrics.ui.screen.song.SongViewModel
-import org.koin.core.module.dsl.factoryOf
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val uiModule = module {
-    single <ScreenStack>{ ScreenStackImpl()  }
+
 }
 
 val vmModule = module {
-    factoryOf(::ListViewModel)
-    factoryOf(::SongViewModel)
-    factoryOf(::MainViewModel)
+    viewModelOf(::ListViewModel)
+    viewModelOf(::SongViewModel)
+    viewModelOf(::MainViewModel)
 }
