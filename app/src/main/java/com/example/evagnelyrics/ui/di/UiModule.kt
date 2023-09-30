10504c8dd5bc58cb29fce16118d1e3f2d18bccfe
“enmanuel” <enmanueldelgado52@gmail.com>
2023-09-30 15:29:00 +0100

    some fixing

--- app/src/main/java/com/example/evagnelyrics/ui/di/UiModule.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/di/UiModule.kt
@@ -6,7 +6,6 @@ import com.example.evagnelyrics.ui.MainViewModel
 import com.example.evagnelyrics.ui.screen.list.ListViewModel
 import com.example.evagnelyrics.ui.screen.song.SongViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
-import org.koin.core.module.dsl.withOptions
 import org.koin.dsl.module
 
 val uiModule = module {
