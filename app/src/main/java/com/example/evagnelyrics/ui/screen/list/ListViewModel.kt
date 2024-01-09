23588a06a448bd06ea11f66bc60b15a40e3ad3ef
“enmanuel” <enmanueldelgado52@gmail.com>
2024-01-09 17:40:40 +0100

    some ui improvement when i filter the list

--- app/src/main/java/com/example/evagnelyrics/ui/screen/list/ListViewModel.kt
+++ app/src/main/java/com/example/evagnelyrics/ui/screen/list/ListViewModel.kt
@@ -7,7 +7,6 @@ import com.example.evagnelyrics.core.Resource
 import com.example.evagnelyrics.domain.model.Lyric
 import com.example.evagnelyrics.domain.usecase.FavoriteUC
 import com.example.evagnelyrics.domain.usecase.GetAllLyricsUC
-import com.example.evagnelyrics.domain.usecase.GetFavoritesUC
 import com.example.evagnelyrics.domain.usecase.GetLyricsByTitleUC
 import com.example.evagnelyrics.ui.player.Player
 import com.example.evagnelyrics.ui.screen.list.model.ListFilter
@@ -17,27 +16,28 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class ListViewModel(
     getAllLyricsUC: GetAllLyricsUC,
-    getFavoritesUC: GetFavoritesUC,
     private val favoriteUC: FavoriteUC,
     private val getLyricsByTitleUC: GetLyricsByTitleUC,
     private val player: Player
 ) : ViewModel() {
-
-    val allLyricsFlow: Flow<List<Lyric>> = getAllLyricsUC()
-
-    val favoritesFlow: Flow<List<Lyric>> = getFavoritesUC()
-
     private val _uiState: Channel<Resource<Unit>> = Channel()
     val uiState get() = _uiState.receiveAsFlow()
 
     private val _filterState = MutableStateFlow(ListFilter())
     val filterState get() = _filterState.asStateFlow()
+
+    val lyricsFlow: Flow<List<Lyric>> = getAllLyricsUC().combine(filterState) { lyrics, filter ->
+        lyrics
+            .filter { lyric -> if (filter.favorite) lyric.favorite else true }
+            .filter { lyric -> lyric.title.contains(filter.searchText, ignoreCase = true) || filter.searchText.isBlank() }
+    }
 
     private val _playerState = MutableStateFlow(PlayerUi())
     val playerState get() = _playerState.asStateFlow()
